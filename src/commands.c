c4905ac8a45a26e2f169c7a03af6891f171ea754
leejeongpil <ijpsm9@gmail.com>
2017-10-09 01:27:54 +0900

    OS_assignment_1

--- src/commands.c
+++ src/commands.c
@@ -1,4 +1,7 @@
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "commands.h"
 
@@ -15,16 +18,65 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
+ char buf[100];
+/*
+ if(argc == 1)
+ {
+  chdir(getenv("HOME"));
+  getcwd(buf, 100);
+  printf("%s\n",buf);
+  return 0;
+ }
+*/
+ if(argc==2)
+ {
+  if((strcmp(argv[0], "cd")))
+  {
+   return 0;
+  }
+  else{
+   if(!chdir(argv[1]))
+   {
+    getcwd(buf, 100);
+    printf("%s\n",buf);
+    return 1;
+   }
+   else{
+    printf("no directory\n");
+    return 0;
+   }
+  }
+ }
+ else
+ {
+  return 0;
+ }
+
   // TODO: Fill it!
-  return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc==1)
+  {
+   if(!(strcmp(argv[0], "pwd")))
+   {
+    char buf[100];
+    getcwd(buf, 100);
+    printf("%s\n",buf);
+    return 1;
+   }
+   else
+   {
+    return 0;
+   }
+  }
+  else
+  {
+   return 0;
+  }
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+// printf("parse start\n");
+ char command_cpy[8096];
+// printf("command_cpy declaration\n");
+ strcpy(command_cpy, command);
+// printf("strcpy\n");
+
+ int n = 0;
+ char *ptr;
+ 
+ ptr=strtok(command_cpy, " \n\t");
+// printf("strtok\n");
+
+ argv[0] = (char**)malloc(sizeof(char*)*10);
+ for(int i=0; i<10; i++)
+ {
+  argv[0][i] = (char*)malloc(sizeof(char)*100);
+ }
+// printf("malloc ok\n");
+
+// printf("while start\n");
+ 
+ while(ptr != NULL)
+ {
+//  printf("while do\n");
+  strcpy(argv[0][n], ptr);
+//  printf("string cpy\n");
+  n++;
+  ptr = strtok(NULL, " \n\t");
+ }
+// printf("while end\n");
+ *argc = n;
+ if(*argc<=0)
+ {
+  *argc = 1;
+  strcpy(argv[0][0],"");
+ }
+ // TODO: Fill this!
 }
